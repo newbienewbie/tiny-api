1fa9f932f2dd35101175cb5f13beb922ef845d75
newbienewbie <webbot@sina.com>
2017-10-10 01:08:27 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# tiny-api
+
+a tiny api library for generate api
+
+## usage
+
+```js
+import {API} from 'tiny-api';
+
+const api=API('role');
+
+/*
+the shape of api is {
+    create:(record)=>{},
+    remove:(id)=>{},
+    update:(id,record)=>{},
+    list:(page,size,condition)=>{},
+}
+*/
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+
+const client=axios.create({
+    withCredentials: true,
+});
+
+export function API(modelName){
+
+    return {
+
+        /**
+         * create model
+         */
+        create:function(record){
+            return client.post(
+                    `/${modelName}/create`,
+                    JSON.stringify(record),
+                    {
+                        headers:{ 'Content-Type':'application/json', }
+                    },
+                ).then(
+                    resp=>resp.data,
+                    (e)=>{throw e;}
+                );
+        },
+
+        /**
+         * remove model
+         */
+        remove:function(id){
+            return client.post(
+                    `/${modelName}/remove`,
+                    JSON.stringify({id}),
+                    {
+                        headers:{ 'Content-Type':'application/json', }
+                    },
+                ).then(
+                    resp=>resp.data,
+                    (e)=>{throw e;}
+                );
+        },
+
+        /**
+         * update model
+         */
+        update:function(id,record){
+            return client.post(
+                    `/${modelName}/update`,
+                    JSON.stringify({id,name,description}),
+                    {
+                        headers:{ 'Content-Type':'application/json', }
+                    },
+                ).then(
+                    resp=>resp.data,
+                    (e)=>{throw e;}
+                );
+        },
+
+
+        /**
+         * list all models by page ,size ,condition
+         */
+        list:function(page=1,size=10,condition={}){
+            return client.post(
+                    `/${modelName}/list`,
+                    JSON.stringify({page,size,condition}),
+                    {
+                        headers:{ 'Content-Type':'application/json', },
+                    }
+                ).then(
+                    resp=>resp.data,
+                    e=>{throw e;}
+                );
+        }
+
+    };
+};
+
+
+export default API;
